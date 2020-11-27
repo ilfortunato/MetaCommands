81df638cd9dd66c5e717f418205a9f3fafe360b2
Mateus Fortunato <mateusfortunato00@gmail.com>
2020-11-26 21:18:48 -0300

    Initial commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommndsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommndsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        // Return all commands 
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }       
+
+        // GET api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandByID(int id)
+        {
+            var commandItem = _repository.GetCommandByID(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command {Id=0, HowTo="Cozinhar ovo", Line="Ferva água", Platform="Cozinha"},
+                new Command {Id=1, HowTo="Cortar pão", Line="Usar faca", Platform="Faca e cozinha?"},
+                new Command {Id=2, HowTo="Fazer chá", Line="Colocar a bolsinha de chá", Platform="Na xícara"},
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandByID(int id)
+        {
+            return new Command {Id=0, HowTo="Boil and egg", Line="Boil water", Platform="Kettle & None"};
+        }
+    }
+}
